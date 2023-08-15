b0d322efa91e74eed89a98fe4c681fc101427b05
Pat Barber <patbarber72@gmail.com>
2023-08-15 11:58:34 +1000

    added home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <main>
+      <header class="brand-name"></header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-test-app';
