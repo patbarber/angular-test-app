2ba9e7a62b003e7fce205faac86b706579e95303
Pat Barber <patbarber72@gmail.com>
2023-08-15 16:15:53 +1000

    changed to formgroup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   template: `
     <section class="grid justify-center">
       <div class="text-center p-4">
         <p class="text-3xl">Add Items To List</p>
       </div>
-      <form class=" rounded-md bg-[#F5A898] p-4 ">
+      <form
+        [formGroup]="itemsForm"
+        class=" rounded-md bg-[#F5A898] p-4"
+        (ngSubmit)="callingFunction()"
+      >
         <div class="grid grid-cols-2 justify-center gap-2 w-full">
-          <input
-            type="text"
-            class="rounded-sm p-2"
-            [(ngModel)]="inputValue"
-            [ngModelOptions]="{ standalone: true }"
-          />
+          <input type="text" class="rounded-sm p-2" formControlName="item" />
 
           <div class="flex justify-center">
             <button
               type="button"
               class="bg-[#15BEE0] rounded-full hover:opacity-70 text-2xl  w-12 h-12"
-              (click)="addItemToList(inputValue)"
+              type="submit"
             >
               +
             </button>
@@ -47,10 +51,26 @@ import { FormControl, FormsModule } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  list = ['test'];
+  list: string[] = [];
   inputValue = '';
 
-  itemFormControl = new FormControl('');
+  itemsForm = new FormGroup({
+    item: new FormControl(''),
+  });
+
+  callingFunction() {
+    console.log(
+      '🚀 ~ file: home.component.ts:62 ~ HomeComponent ~ callingFunction ~ itemsForm:',
+      this.itemsForm.value.item
+    );
+    if (
+      this.itemsForm.value.item === undefined ||
+      this.itemsForm.value.item === null
+    )
+      return;
+
+    this.list.push(this.itemsForm.value.item);
+  }
 
   addItemToList(value: string): void {
     this.list.push(value);
